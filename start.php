8ab3454c42a01469db71da43336196a63521b68e
ericchan378 <ericchan378@gmail.com>
2024-01-25 23:37:47 -0500

    Create start.php
    Creates the textbox and search button

---
+++ start.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        
+        
+        
+        
+        <form action = "search_engine.php" method = "GET">
+            
+            <input class = "searchBox" type="text" name = "term">
+            <input class = "searchButton" type = "submit" value = "Search">
+            
+        </form>
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
